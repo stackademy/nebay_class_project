67f218d0a83f8ca10d47eec221652ec2a253d4f2
Stackademy <hello@stackademy.com>
2014-10-27 17:18:29 +0100

    home page update

--- app/uploaders/auction_image_uploader.rb
+++ app/uploaders/auction_image_uploader.rb
@@ -33,7 +33,7 @@ class AuctionImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [200, 200]
+    process :resize_to_fit => [400, 400]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
