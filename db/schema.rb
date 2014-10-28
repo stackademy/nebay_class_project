49539ce0c84c2b42ecee5343cab118adc68b0006
stevenmarc <stevenmarcfr@gmail.com>
2014-10-28 16:21:53 +0100

    added check auction winner

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027131236) do
+ActiveRecord::Schema.define(version: 20141028143106) do
 
   create_table "auctions", force: true do |t|
     t.string   "title"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20141027131236) do
     t.datetime "updated_at"
     t.string   "category"
     t.string   "picture"
+    t.boolean  "paid",        default: false
   end
 
   create_table "bids", force: true do |t|
