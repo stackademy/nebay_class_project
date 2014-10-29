2d251cb37c77b569c0b3e7323b653f350beb4f2e
Robin <robin.roemer@gmail.com>
2014-10-29 13:02:19 +0100

    added new styles to the homepage picture display

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,7 +1,14 @@
 class Auction < ActiveRecord::Base
   has_many :bids
   belongs_to :user
-  mount_uploader :picture , AuctionImageUploader 
+  mount_uploader :picture , AuctionImageUploader
+  validates_presence_of :title, :message => "can't be empty"
+  validates_presence_of :description, :message => "can't be empty"
+  validates_presence_of :category, :message => "can't be empty"
+  validates_presence_of :picture, :message => "can't be empty"
+
+
+
 
 	 def highest_bid
 	    self.bids.order(amount: :desc).first
